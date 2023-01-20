f19171e61f50c5858ee816b7404e5869c4282d6f
lucas8708 <lukkasjralmeida2@gmail.com>
2023-01-19 23:13:47 -0300

    subindo arquitetura MVC

---
+++ backend/controller/Categoria.js
@@ -0,0 +1,44 @@
+const sequelize = require('sequelize');
+const model = require('../models');
+const categoria = model.Categoria;
+
+module.exports = {
+    async create(request, response) {
+        try {
+            const {
+                descricao
+            } = request.body
+
+            const Categoria = await categoria.create({
+                descricao
+            });
+
+            return response.json({ msg: "Categoria cadastrada com sucesso!! " });
+
+        } catch (error) {
+            return response.json({ msg: "Não foi possivel cadastrar a categoria " + error });
+
+        }
+    },
+
+    async update(request, response) {
+        try {
+            const { id } = request.params;
+
+            const {
+                descricao
+            } = request.body
+
+            const Categoria = await categoria.update({
+                descricao
+
+            }, { where: { id } });
+
+            return response.json({ msg: "Categoria alterada com sucesso!! " });
+
+
+        } catch (error) {
+            return response.json({ msg: "Não foi possivel alterar a categoria " + error });
+        }
+    }
+}
